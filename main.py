190979516f76be3d2e7a14565303313e1eb98598
Ethan Nitz <etnitz@gmail.com>
2022-09-14 14:34:07 -0700

    made all buttons

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def add_text(a):
     entryField.insert('1.0', entry)
 
 window = Tk()
-window.geometry('500x500')
+window.geometry('495x495')
 window.title('Calculator')
 
 entryField = Text(window, width=61, height=5)
@@ -30,6 +30,46 @@ four.grid(row=2,column=4)
 five = Button(window, text='5', command=lambda: add_text(5))
 five.grid(row=3,column=1)
 
+six = Button(window, text='6', command=lambda: add_text(6))
+six.grid(row=3,column=2)
 
+seven = Button(window, text='7', command=lambda: add_text(7))
+seven.grid(row=3,column=3)
+
+eight = Button(window, text='8', command=lambda: add_text(8))
+eight.grid(row=3,column=4)
+
+nine = Button(window, text='9', command=lambda:add_text(9))
+nine.grid(row=4,column=1)
+
+zero = Button(window, text='0', command=lambda: add_text(0))
+zero.grid(row=4,column=2)
+
+add = Button(window, text='+', command=lambda: add_text('+'))
+add.grid(row=4,column=3)
+
+sub = Button(window, text='-', command=lambda: add_text('-'))
+sub.grid(row=4,column=4)
+
+mult = Button(window, text='*', command=lambda: add_text('*'))
+mult.grid(row=5, column=1)
+
+div = Button(window, text='/', command=lambda: add_text('/'))
+div.grid(row=5, column=2)
+
+lp = Button(window, text='(', command=lambda: add_text('('))
+lp.grid(row=5,column=3)
+
+rp = Button(window, text=')', command=lambda: add_text(')'))
+rp.grid(row=5, column=4)
+
+dec = Button(window, text='.', command=lambda: add_text('.'))
+dec.grid(row=6, column=1)
+
+equal = Button(window, text='=')
+equal.grid(row=6, column=2)
+
+clear = Button(window, text='clear')
+clear.grid(row=6, column=3)
 
 window.mainloop()
